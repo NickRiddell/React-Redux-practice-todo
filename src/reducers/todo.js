e7543d59451d2313fe3e17d51de21ef4e8a2c3bb
Nick Riddell <nickjriddell@gmail.com>
2017-09-21 20:36:57 +0100

    delete functionality added

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import {getTodos, createTodos, updateTodo} from '../lib/todoServices'
+import {getTodos, createTodos, updateTodo, destroyTodo} from '../lib/todoServices'
 import {showMessage} from './messages'
 const initState = {
     todos: [],
@@ -9,11 +9,13 @@ export const TODO_ADD = 'TODO_ADD'
 export const TODOS_LOAD = 'TODO_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 export const TODO_REPLACE = 'TODO_REPLACE'
+export const TODO_REMOVE = 'TODO_REMOVE'
 
 export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload: val})
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 export const replaceTodo = (todo) => ({type: TODO_REPLACE, payload: todo})
+export const removeTodo = (id) => ({type: TODO_REMOVE, payload: id})
 export const fetchTodos = () => {
   return (dispatch) => {
     dispatch(showMessage('Loading Todos'))
@@ -41,6 +43,14 @@ export const toggleTodo = (id) => {
   }
 }
 
+export const deleteTodo = (id) => {
+  return (dispatch) => {
+    dispatch(showMessage('Removing Todo'))
+    destroyTodo(id)
+      .then(() => dispatch(removeTodo(id)))
+  }
+}
+
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
@@ -53,6 +63,10 @@ export default (state = initState, action) => {
       return {...state,
         todos: state.todos
           .map(t => t.id === action.payload.id ? action.payload : t)
+      }
+    case TODO_REMOVE:
+      return {...state,
+        todos: state.todos.filter(t => t.id !== action.payload)
       }
     default:
       return state
