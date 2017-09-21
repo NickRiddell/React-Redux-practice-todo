0abff9761df3878c345f0caf212a011283d8b3e6
Nick Riddell <nickjriddell@gmail.com>
2017-09-21 19:55:27 +0100

    saving and loading messages added and new message reducer

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,11 +1,12 @@
 import {getTodos, createTodos} from '../lib/todoServices'
+import {showMessage} from './messages'
 const initState = {
     todos: [],
     currentTodo: ''
 }
 
-const TODO_ADD = 'TODO_ADD'
-const TODOS_LOAD = 'TODO_LOAD'
+export const TODO_ADD = 'TODO_ADD'
+export const TODOS_LOAD = 'TODO_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
 export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload: val})
@@ -13,6 +14,7 @@ export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 export const fetchTodos = () => {
   return (dispatch) => {
+    dispatch(showMessage('Loading Todos'))
     getTodos()
       .then(todos => dispatch(loadTodos(todos)))
   }
@@ -20,6 +22,7 @@ export const fetchTodos = () => {
 
 export const saveTodo = (name) => {
   return(dispatch) => {
+    dispatch(showMessage('Saving Todo'))
     createTodos(name)
       .then(res => dispatch(addTodo(res)))
   }
