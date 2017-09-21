9271bda8ea8204d3fea5a85ad8734a682ee90af6
Nick Riddell <nickjriddell@gmail.com>
2017-09-21 20:22:49 +0100

    update functionality added with toggling todos

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import {getTodos, createTodos} from '../lib/todoServices'
+import {getTodos, createTodos, updateTodo} from '../lib/todoServices'
 import {showMessage} from './messages'
 const initState = {
     todos: [],
@@ -8,10 +8,12 @@ const initState = {
 export const TODO_ADD = 'TODO_ADD'
 export const TODOS_LOAD = 'TODO_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
+export const TODO_REPLACE = 'TODO_REPLACE'
 
 export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload: val})
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
+export const replaceTodo = (todo) => ({type: TODO_REPLACE, payload: todo})
 export const fetchTodos = () => {
   return (dispatch) => {
     dispatch(showMessage('Loading Todos'))
@@ -28,6 +30,17 @@ export const saveTodo = (name) => {
   }
 }
 
+export const toggleTodo = (id) => {
+  return (dispatch, getState) => {
+    dispatch(showMessage('Saving todo update'))
+    const {todos} = getState().todo
+    const todo = todos.find(t => t.id === id)
+    const toggled = {...todo, isComplete: !todo.isComplete}
+    updateTodo(toggled)
+      .then(res => dispatch(replaceTodo(res)))
+  }
+}
+
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
@@ -36,6 +49,11 @@ export default (state = initState, action) => {
       return {...state, todos: action.payload}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
+    case TODO_REPLACE:
+      return {...state,
+        todos: state.todos
+          .map(t => t.id === action.payload.id ? action.payload : t)
+      }
     default:
       return state
   }
